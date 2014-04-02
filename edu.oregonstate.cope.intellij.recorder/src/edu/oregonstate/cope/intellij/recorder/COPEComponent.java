b58991757d8322b477161eb995f5d0b939635545
Michael Hilton <michael.hilton@gmail.com>
2014-04-02 10:56:11 -0700

    Cleanup

--- edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/COPEComponent.java
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/COPEComponent.java
@@ -22,12 +22,6 @@ public class COPEComponent implements ApplicationComponent {
     }
 
     public void initComponent() {
-//        ChangePersister changePersister = ChangePersister.instance();
-//        changePersister.setFileManager(new SimpleFileProvider("./test.json"));
-//
-//        recorder = new ClientRecorder();
-//        EditorFactory.getInstance().addEditorFactoryListener(new EditorFactoryListener(recorder));
-
         status = new COPEStatus();
 
         ProjectManager.getInstance().addProjectManagerListener(new ProjectManagerAdapter() {
@@ -37,45 +31,9 @@ public class COPEComponent implements ApplicationComponent {
                 if(statusBar != null){
                     statusBar.addWidget(status);
                 }
-
                 Survey dialog = new Survey();
                 dialog.pack();
                 dialog.setVisible(true);
-//                String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
-//                Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
-
-
-
-
-                /*
-                JFrame.setDefaultLookAndFeelDecorated(true);
-                JFrame frame = new JFrame("[=] There's a JPanel in here! [=]");
-
-                JPanel jp = new JPanel(new SpringLayout());
-                jp.setOpaque(true);
-
-                frame.setContentPane(jp);
-
-                JLabel lTitle = new JLabel("Thanks you for participating in the study titled: Understanding TDD Practices.  Please answer just a few demographic questions before you begin using the plugin",JLabel.TRAILING);
-                jp.add(lTitle);
-
-                JLabel l = new JLabel("Name:",JLabel.TRAILING);
-                jp.add(l);
-                JTextField textField = new JTextField(10);
-                l.setLabelFor(textField);
-                jp.add(textField);
-
-
-                //SpringUtilities.makeCompactGrid(jp,2,2,6,6,6,6);
-
-
-
-                // The other bits and pieces that make our program a bit more stable.
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(290, 100);
-                frame.setVisible(true);
-                */
-
             }
 
             @Override
@@ -83,20 +41,6 @@ public class COPEComponent implements ApplicationComponent {
 
             }
         });
-
-/*    This will be an example for survey
-      ActionManager am = ActionManager.getInstance();
-        TextBoxes action = new TextBoxes();
-        // Passes an instance of your custom TextBoxes class to the registerAction method of the ActionManager class.
-        am.registerAction("MyPluginAction", action);
-        // Gets an instance of the WindowMenu action group.
-        DefaultActionGroup windowM = (DefaultActionGroup) am.getAction("WindowMenu");
-        // Adds a separator and a new menu command to the WindowMenu group on the main menu.
-        windowM.addSeparator();
-        windowM.add(action);
-      */
-
-
     }
 
     public void disposeComponent() {
