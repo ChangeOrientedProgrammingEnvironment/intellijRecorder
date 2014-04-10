aa906554b6fa201cf582251b508fa35bc3d26017
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-09 17:03:41 -0700

    Added IDEA events, JSON constants and recorder
    This supplements the initial recorder for our own purposes.
    
    Is this a good idea? I don't know, but I do not want to pollute the
    upstream with IDEA specific crap. So it will have to do for now.

---
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/IDEAEvents.java
@@ -0,0 +1,10 @@
+package edu.oregonstate.cope.intellij.recorder;
+
+import edu.oregonstate.cope.clientRecorder.Events;
+
+/**
+ * Created by caius on 4/9/14.
+ */
+public enum IDEAEvents{
+    ideaLaunch;
+}
---
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/IdeaJsonConstants.java
@@ -0,0 +1,9 @@
+package edu.oregonstate.cope.intellij.recorder;
+
+/**
+ * Created by caius on 4/9/14.
+ */
+public class IdeaJsonConstants {
+
+    public final static String IDEA_LAUNCH_XML = "ideaLaunchXML";
+}
