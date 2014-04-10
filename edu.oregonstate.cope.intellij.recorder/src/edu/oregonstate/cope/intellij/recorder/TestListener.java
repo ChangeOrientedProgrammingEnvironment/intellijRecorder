61955a209222b72be21fed987a56ca5844a3dc13
cdmihai <codobanmihai@gmail.com>
2014-04-10 16:31:56 -0700

    Brought in eclipse code
    Copy paste the Result class. No use making the same thing.

--- edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/TestListener.java
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/TestListener.java
@@ -10,6 +10,29 @@ import com.intellij.openapi.project.ProjectManager;
  * Created by mihai on 4/8/14.
  */
 public class TestListener extends TestStatusListener{
+
+    //copy paste from org.eclipse.jdt.junit.model.ITestElement.Result
+    public static final class Result {
+        /** state that describes that the test result is undefined */
+        public static final Result UNDEFINED= new Result("Undefined"); //$NON-NLS-1$
+        /** state that describes that the test result is 'OK' */
+        public static final Result OK= new Result("OK"); //$NON-NLS-1$
+        /** state that describes that the test result is 'Error' */
+        public static final Result ERROR= new Result("Esrror"); //$NON-NLS-1$
+        /** state that describes that the test result is 'Failure' */
+        public static final Result FAILURE= new Result("Failure"); //$NON-NLS-1$
+        /** state that describes that the test result is 'Ignored' */
+        public static final Result IGNORED= new Result("Ignored"); //$NON-NLS-1$
+
+        private String fName;
+        private Result(String name) {
+            fName= name;
+        }
+        public String toString() {
+            return fName;
+        }
+    }
+
     @Override
     public void testSuiteFinished(AbstractTestProxy root) {
         int i = 1;
