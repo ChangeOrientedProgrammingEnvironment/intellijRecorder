9aee4c92f2c8e549d2d8e3ab57ddb9f7e31589c6
Michael Hilton <michael.hilton@gmail.com>
2014-04-21 10:48:06 -0700

    added new files

---
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/COPEConfigManager.java
@@ -0,0 +1,7 @@
+package edu.oregonstate.cope.intellij.recorder;
+
+/**
+ * Created by michaelhilton on 4/15/14.
+ */
+public class COPEConfigManager {
+}
---
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/CheckRESTVersion.java
@@ -0,0 +1,7 @@
+package edu.oregonstate.cope.intellij.recorder;
+
+/**
+ * Created by michaelhilton on 4/17/14.
+ */
+public class CheckRESTVersion {
+}
