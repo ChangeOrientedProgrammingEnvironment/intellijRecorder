bfa43ddbb54f0549e06de60fdcb5dbee8bf3824b
Caius Brindescu <brindesc@eecs.oregonstate.edu>
2014-04-23 16:50:35 -0700

    Keeping track of the documents and the listeners I registered
    Because "IDEA" can't give you that info... And if you try to register
    the same listener twice, it will log an error, throw an uncatchable
    Throwable and... still registers the listener.
    
    Because hey, if I have an error, let's just keep going... What could
    possibly go wrong?

--- edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/listeners/EditorFactoryListener.java
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/listeners/EditorFactoryListener.java
@@ -8,6 +8,9 @@ import edu.oregonstate.cope.clientRecorder.ClientRecorder;
 import edu.oregonstate.cope.intellij.recorder.COPEComponent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by caius on 3/3/14.
  */
@@ -17,9 +20,14 @@ public class EditorFactoryListener implements com.intellij.openapi.editor.event.
     private ClientRecorder recorder;
     private String basePath;
 
+    private Map<Document, DocumentListener> listenerMap;
+    private Map<String, Document> documentMap;
+
     public EditorFactoryListener(COPEComponent copeComponent, ClientRecorder recorder) {
         this.copeComponent = copeComponent;
         this.recorder = recorder;
+        this.listenerMap = new HashMap<Document, DocumentListener>();
+        this.documentMap = new HashMap<String, Document>();
     }
 
     @Override
@@ -31,9 +39,11 @@ public class EditorFactoryListener implements com.intellij.openapi.editor.event.
             return;
         }
 
-        Document document = event.getEditor().getDocument();
-        DocumentListener documentListener = new DocumentListener(filePath, copeComponent.getCommandListener(), recorder);
+        final Document document = event.getEditor().getDocument();
+        final DocumentListener documentListener = new DocumentListener(filePath, copeComponent.getCommandListener(), recorder);
         document.addDocumentListener(documentListener);
+        documentMap.put(filePath, document);
+        listenerMap.put(document, documentListener);
 
         recorder.recordFileOpen(filePath);
     }
@@ -54,7 +64,11 @@ public class EditorFactoryListener implements com.intellij.openapi.editor.event.
     @Override
     public void editorReleased(@NotNull EditorFactoryEvent event) {
         String filePath = getPathOfAffectedFile(event);
-
         recorder.recordFileClose(filePath);
+
+        Document document = documentMap.get(filePath);
+        System.out.println(filePath);
+        DocumentListener documentListener = listenerMap.get(document);
+        document.removeDocumentListener(documentListener);
     }
 }
