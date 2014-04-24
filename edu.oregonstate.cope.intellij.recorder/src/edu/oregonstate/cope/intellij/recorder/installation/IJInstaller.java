f41e88ba0e1f1ead047e61c72fb5a46960a03946
cdmihai <codobanmihai@gmail.com>
2014-04-24 10:16:13 -0700

    The IJInstaller

--- edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/installation/IJInstaller.java
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/installation/IJInstaller.java
@@ -1,7 +1,7 @@
 package edu.oregonstate.cope.intellij.recorder.installation;
 
-import edu.oregonstate.cope.clientRecorder.installer.Installer;
-import edu.oregonstate.cope.clientRecorder.installer.InstallerOperation;
+import edu.oregonstate.cope.clientRecorder.RecorderFacadeInterface;
+import edu.oregonstate.cope.clientRecorder.installer.*;
 
 import java.util.ArrayList;
 
@@ -9,8 +9,19 @@ import java.util.ArrayList;
  * Created by mihai on 4/24/14.
  */
 public class IJInstaller extends Installer {
+
+    public IJInstaller(RecorderFacadeInterface recorder, InstallerHelper installerHelper) {
+        super(recorder, installerHelper);
+    }
+
     @Override
     protected ArrayList<InstallerOperation> getInstallOperations() {
-        return null;
+        ArrayList<InstallerOperation> operations = new ArrayList<>();
+
+        operations.add(new ConfigInstallOperation());
+        operations.add(new IJSurveyOperation());
+        operations.add(new EmailInstallOperation());
+
+        return operations;
     }
 }
