ba1b850d1264bffcd711ac21f46282766ab498b3
cdmihai <codobanmihai@gmail.com>
2014-04-24 10:24:45 -0700

    The IJInstallerHelper

--- edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/installation/IJInstallerHelper.java
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/installation/IJInstallerHelper.java
@@ -1,18 +1,26 @@
 package edu.oregonstate.cope.intellij.recorder.installation;
 
 import edu.oregonstate.cope.clientRecorder.installer.InstallerHelper;
+import edu.oregonstate.cope.intellij.recorder.COPEComponent;
 
 /**
  * Created by mihai on 4/24/14.
  */
 public class IJInstallerHelper implements InstallerHelper {
+
+    private COPEComponent component;
+
+    public void IJInstallerHelper(COPEComponent component){
+        this.component = component;
+    }
+
     @Override
     public String getPluginVersion() {
-        return null;
+        return component.getPluginVersion();
     }
 
     @Override
     public void takeSnapshotOfAllProjects() {
-
+        component.takeSnapshotOfProject();
     }
 }
