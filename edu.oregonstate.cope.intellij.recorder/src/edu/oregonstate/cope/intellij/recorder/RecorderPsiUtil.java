994c90490d748c868af3b8c6c0c849cef1334017
cdmihai <codobanmihai@gmail.com>
2014-05-08 18:18:01 -0700

    Construct the qualified name of a psi element

---
+++ edu.oregonstate.cope.intellij.recorder/src/edu/oregonstate/cope/intellij/recorder/RecorderPsiUtil.java
@@ -0,0 +1,80 @@
+package edu.oregonstate.cope.intellij.recorder;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMember;
+import com.intellij.psi.PsiQualifiedNamedElement;
+import com.intellij.psi.util.PsiUtil;
+import com.intellij.psi.util.PsiUtilCore;
+import org.codehaus.groovy.runtime.ReverseListIterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by mihai on 5/8/14.
+ */
+public class RecorderPsiUtil {
+    public static String getQualifiedName(PsiElement psiElement){
+
+        //if (psiElement instanceof PsiMember)
+        //    return PsiUtil.getMemberQualifiedName((PsiMember) psiElement);
+
+        if(psiElement instanceof PsiQualifiedNamedElement)
+            return ((PsiQualifiedNamedElement)psiElement).getQualifiedName();
+
+        List<PsiElement> ancestors = getPathToRoot(psiElement);
+
+        List<String> nameFragments = new ArrayList<>();
+
+        for (PsiElement pathElement : ancestors){
+            if(pathElement instanceof PsiQualifiedNamedElement){
+                nameFragments.add(((PsiQualifiedNamedElement) pathElement).getQualifiedName());
+                break;
+            }
+
+            String simpleName = PsiUtilCore.getName(pathElement);
+
+            if (simpleName != null)
+                nameFragments.add(simpleName);
+            else
+                continue;
+        }
+
+        String qualifiedName = "";
+
+        ReverseListIterator<String> iterator = new ReverseListIterator<>(nameFragments);
+
+        while (iterator.hasNext()){
+            qualifiedName += iterator.next() + ".";
+        }
+
+        if(PsiUtilCore.getName(psiElement) != null)
+            qualifiedName += PsiUtilCore.getName(psiElement);
+        else
+            qualifiedName += "unnamed";
+
+        return qualifiedName;
+    }
+
+    /**
+     * Does not contain initial element
+     * Order: from element to root
+     * @param psiElement
+     * @return
+     */
+    private static List<PsiElement> getPathToRoot(PsiElement psiElement) {
+        List<PsiElement> pathToRoot = new ArrayList<>();
+
+        psiElement = psiElement.getParent();
+
+        while (psiElement != null){
+            pathToRoot.add(psiElement);
+            psiElement = psiElement.getParent();
+        }
+
+        return pathToRoot;
+    }
+
+}
